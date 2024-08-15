0dc9bec265d544cc4517d267a3cf3315686ed338
sonhasoftware <sonha.software.1@gmail.com>
2024-08-15 11:07:22 +0700

    update code SSP

--- hr_zk_attendance/models/biometric_device_details.py
+++ hr_zk_attendance/models/biometric_device_details.py
@@ -4,6 +4,7 @@ import logging
 import pytz
 from odoo import fields, models, _
 from odoo.exceptions import UserError, ValidationError
+from datetime import timedelta
 
 _logger = logging.getLogger(__name__)
 try:
@@ -13,7 +14,6 @@ except ImportError:
 
 
 class BiometricDeviceDetails(models.Model):
-    """Model for configuring and connect the biometric device with odoo"""
     _name = 'biometric.device.details'
     _description = 'Biometric Device Details'
 
@@ -30,7 +30,6 @@ class BiometricDeviceDetails(models.Model):
                                  help='Current Company')
 
     def device_connect(self, zk):
-        """Function for connecting the device with Odoo"""
         try:
             conn = zk.connect()
             return conn
@@ -38,7 +37,6 @@ class BiometricDeviceDetails(models.Model):
             return False
 
     def action_test_connection(self):
-        """Checking the connection status"""
         zk = ZK(self.device_ip, port=self.port_number, timeout=30,
                 password=False, ommit_ping=False)
         try:
@@ -56,8 +54,6 @@ class BiometricDeviceDetails(models.Model):
             raise ValidationError(f'{error}')
 
     def action_clear_attendance(self):
-        """Methode to clear record from the zk.machine.attendance model and
-        from the device"""
         for info in self:
             try:
                 machine_ip = info.device_ip
@@ -92,83 +88,46 @@ class BiometricDeviceDetails(models.Model):
                 raise ValidationError(f'{error}')
 
     def action_download_attendance(self):
-        """Function to download attendance records from the device"""
         _logger.info("++++++++++++Cron Executed++++++++++++++++++++++")
-        zk_attendance = self.env['zk.machine.attendance']
-        hr_attendance = self.env['hr.attendance']
+        master_attendance = self.env['master.data.attendance']
+        hr_employee = self.env['hr.employee']
         for info in self:
             machine_ip = info.device_ip
             zk_port = info.port_number
             try:
-                # Connecting with the device with the ip and port provided
-                zk = ZK(machine_ip, port=zk_port, timeout=15,
-                        password=0,
-                        force_udp=False, ommit_ping=False)
+                zk = ZK(machine_ip, port=zk_port, timeout=None, password=0, force_udp=False, ommit_ping=False)
             except NameError:
-                raise UserError(
-                    _("Pyzk module not Found. Please install it"
-                      "with 'pip3 install pyzk'."))
+                raise UserError(_("Pyzk module not Found. Please install it with 'pip3 install pyzk'."))
             conn = self.device_connect(zk)
             if conn:
                 conn.disable_device()
                 users = conn.get_users()
                 attendances = conn.get_attendance()
                 if attendances:
-                    # Thu thập dữ liệu chấm công theo nhân viên và ngày
-                    attendance_by_employee_date = {}
+                    # Thu thập dữ liệu chấm công theo nhân viên và ngày trong khoảng thời gian từ tháng 6 năm 2024 đến hiện tại
                     for each in attendances:
                         atten_time = each.timestamp
-                        if atten_time.year == 2024:
-                            date_key = atten_time.date()
+                        if atten_time.year == 2024 and atten_time.month >= 6:
                             user_key = each.user_id
-                            if (user_key, date_key) not in attendance_by_employee_date:
-                                attendance_by_employee_date[(user_key, date_key)] = {'check_in': None,
-                                                                                     'check_out': None}
-                            if each.punch == 0:  # check-in
-                                attendance_by_employee_date[(user_key, date_key)]['check_in'] = atten_time
-                            elif each.punch == 1:  # check-out
-                                attendance_by_employee_date[(user_key, date_key)]['check_out'] = atten_time
 
-                    # Xử lý dữ liệu chấm công đã thu thập
-                    for user in users:
-                        user_key = user.user_id
-                        employee = self.env['hr.employee'].search([('device_id_num', '=', user_key)])
-                        if not employee:
-                            employee = self.env['hr.employee'].create({
-                                'device_id_num': user_key,
-                                'name': user.name
-                            })
+                            # Trừ 7 giờ từ thời gian chấm công
+                            atten_time_adjusted = atten_time - timedelta(hours=7)
 
-                        for date_key in attendance_by_employee_date.keys():
-                            if (user_key, date_key) in attendance_by_employee_date:
-                                times = attendance_by_employee_date[(user_key, date_key)]
-                                check_in_time = times['check_in']
-                                check_out_time = times['check_out']
+                            # Tìm hoặc tạo nhân viên
+                            employee = hr_employee.search([('device_id_num', '=', user_key)])
 
-                                if check_in_time and check_out_time:
-                                    local_tz = pytz.timezone(self.env.user.partner_id.tz or 'GMT')
+                            # Kiểm tra xem dữ liệu chấm công đã tồn tại chưa
+                            existing_attendance = master_attendance.search([
+                                ('employee_id', '=', employee.id),
+                                ('attendance_time', '=', fields.Datetime.to_string(atten_time_adjusted))
+                            ])
 
-                                    local_check_in = local_tz.localize(check_in_time, is_dst=None)
-                                    utc_check_in = local_check_in.astimezone(pytz.utc)
-                                    check_in_str = utc_check_in.strftime("%Y-%m-%d %H:%M:%S")
-                                    check_in_time = fields.Datetime.to_string(
-                                        datetime.datetime.strptime(check_in_str, "%Y-%m-%d %H:%M:%S"))
-
-                                    local_check_out = local_tz.localize(check_out_time, is_dst=None)
-                                    utc_check_out = local_check_out.astimezone(pytz.utc)
-                                    check_out_str = utc_check_out.strftime("%Y-%m-%d %H:%M:%S")
-                                    check_out_time = fields.Datetime.to_string(
-                                        datetime.datetime.strptime(check_out_str, "%Y-%m-%d %H:%M:%S"))
-
-                                    # Đảo ngược thời gian nếu cần thiết
-                                    if check_out_time < check_in_time:
-                                        check_in_time, check_out_time = check_out_time, check_in_time
-
-                                    hr_attendance.create({
-                                        'employee_id': employee.id,
-                                        'check_in': check_in_time,
-                                        'check_out': check_out_time
-                                    })
+                            # Lưu dữ liệu vào master.data.attendance nếu chưa tồn tại
+                            if employee and not existing_attendance:
+                                master_attendance.sudo().create({
+                                    'employee_id': employee.id,
+                                    'attendance_time': fields.Datetime.to_string(atten_time_adjusted)
+                                })
                     conn.disconnect()
                     return True
                 else:
