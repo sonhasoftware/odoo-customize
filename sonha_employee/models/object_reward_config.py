3f000feea8fe86e87c529d5ec7ca1cccab4d4397
LongNguyn <157455862+LongNguyn@users.noreply.github.com>
2024-10-25 13:47:46 +0700

    update kỷ luật, khen thưởng

--- sonha_employee/models/object_reward_config.py
+++ sonha_employee/models/object_reward_config.py
@@ -7,3 +7,4 @@ class ObjectReward(models.Model):
     #
     id = fields.Char(string="ID đối tượng", required = True)
     name = fields.Char(string="Tên đối tượng khen thưởng")
+    type = fields.Selection([('unit', 'Đơn vị'), ('person', 'Cá nhân')], string="Loại đối tượng")
