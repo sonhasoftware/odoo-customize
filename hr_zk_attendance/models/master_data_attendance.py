842c9d9fe63c709286609041cdbe4ecee3fb04af
sonhasoftware <sonha.software.1@gmail.com>
2024-11-04 14:40:00 +0700

    update chấm công thô

--- hr_zk_attendance/models/master_data_attendance.py
+++ hr_zk_attendance/models/master_data_attendance.py
@@ -1,8 +1,9 @@
 from odoo import models, fields
 
+
 class MasterDataAttendance(models.Model):
     _name = 'master.data.attendance'
     _description = 'Master Data Attendance'
 
-    employee_id = fields.Many2one('hr.employee', string='Employee', required=True)
-    attendance_time = fields.Datetime(string='Attendance Time', required=True)
+    employee_id = fields.Many2one('hr.employee', string='Nhân viên', required=True)
+    attendance_time = fields.Datetime(string='Thời gian', required=True)
