429c15cb827821128c02dc9fac8af86147f01452
Vũ Thế Huynh <kamevu77@gmail.com>
2024-11-25 09:05:07 +0700

    kéo dữ liệu loại đơn mua

--- sonha_purchase/models/__init__.py
+++ sonha_purchase/models/__init__.py
@@ -4,3 +4,4 @@ from . import x_type_budget
 from . import x_mch_list
 from . import x_hr_validate_person
 from . import x_hr_validate_procedure
+from . import x_type_po
---
+++ sonha_purchase/models/x_type_po.py
@@ -0,0 +1,24 @@
+from odoo import models, fields, _
+
+class XTypePO(models.Model):
+    _name = 'x.type.po'
+
+    name = fields.Char(string="Loại đơn mua")
+    x_supply = fields.Boolean(string="Ban cung ứng mua hộ")
+
+    def sync_x_type_po_data(self):
+        connector = self.env['external.db.connector'].sudo()
+        query = "SELECT name,x_supply FROM x_type_po"
+
+        data = connector.execute_query(query)
+        records_to_create = []
+
+        if data:
+            for r in data:
+                records_to_create.append({
+                    'name': r.get('name'),
+                    'x_supply': r.get('x_supply')
+                })
+
+        if records_to_create:
+            self.sudo().create(records_to_create)
