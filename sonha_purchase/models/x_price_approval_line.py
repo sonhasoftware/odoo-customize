244b2a05a12b8376f259282ef139afd6f51e524d
Vũ Thế Huynh <kamevu77@gmail.com>
2024-12-01 21:50:07 +0700

    update Chi tiết yêu cầu duyệt giá

--- sonha_purchase/models/x_price_approval_line.py
+++ sonha_purchase/models/x_price_approval_line.py
@@ -4,7 +4,7 @@ from odoo import models, fields,_
 class XPriceApprovalLine(models.Model):
     _name = 'x.price.approval.line'
 
-    # x_pp_id = fields.Many2one('x.price.approval',string="Duyệt giá")
+    x_pp_id = fields.Many2one('x.price.approval',string="Duyệt giá")
     # x_reference_price_id = fields.Many2one('x.reference.price',string="Kho nhận hàng - Tên sản phẩm")
     # x_company_id = fields.Many2one('res.company',string="Đơn vị đặt")
     # x_product_id = fields.Many2one('product.product',string="Sản phẩm")
@@ -12,13 +12,29 @@ class XPriceApprovalLine(models.Model):
     # x_supplier_selected_id = fields.Many2one('res.partner',string="NCC được chọn")
     x_ord_qty = fields.Float(string="Số lượng yêu cầu")
     # x_currency_id = fields.Many2one('res.currency',string="Tiền tệ")
-    # x_supplier_price = fields.Monetary(string="Đơn giá", currency_field='x_currency_id')
+    # x_supplier_price = fields.Monetary(string="Đơn giá")
     x_total = fields.Float(string="Tổng")
-    # x_currency2_id = fields.Many2one('res.currency',string="X Currency2")
+    # x_currency2_id = fields.Many2one(comodel_name='res.currency', default=lambda self: self.env.ref('base.VND'))
 
     def sync_x_price_approval_line_data(self):
         connector = self.env['external.db.connector'].sudo()
-        query = "SELECT id,x_ord_qty,x_total FROM x_price_approval_line"
+        query = """
+            SELECT 
+                id,
+                x_pp_id,
+                x_reference_price_id,
+                x_company_id,
+                x_product_id,
+                x_product_uom_id,
+                x_supplier_selected_id,
+                x_ord_qty,
+                x_currency_id,
+                x_supplier_price,
+                x_total,
+                x_currency2_id
+            FROM 
+                x_price_approval_line
+        """
 
         data = connector.execute_query(query)
         records_to_create = []
@@ -28,8 +44,17 @@ class XPriceApprovalLine(models.Model):
             self.with_context(active_test=False).sudo().search([]).unlink()
             for r in data:
                 records_to_create.append({
+                    'x_pp_id': r.get('x_pp_id'),
+                    # 'x_reference_price_id': r.get('x_reference_price_id'),
+                    # 'x_company_id': r.get('x_company_id'),
+                    # 'x_product_id': r.get('x_product_id'),
+                    # 'x_product_uom_id': r.get('x_product_uom_id'),
+                    # 'x_supplier_selected_id': r.get('x_supplier_selected_id'),
                     'x_ord_qty': r.get('x_ord_qty'),
-                    'x_total': r.get('x_total')
+                    # 'x_currency_id': r.get('x_currency_id'),
+                    # 'x_supplier_price': r.get('x_supplier_price'),
+                    'x_total': r.get('x_total'),
+                    # 'x_currency2_id' : r.get('x_currency2_id'),
                 })
 
         if records_to_create:
