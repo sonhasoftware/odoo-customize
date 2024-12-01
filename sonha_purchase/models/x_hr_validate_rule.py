4924d478f1dba0e464df56872c4965a5357fbb34
Vũ Thế Huynh <kamevu77@gmail.com>
2024-12-01 21:57:43 +0700

    update Quy tắc phê duyệt: gỡ các trường đã có dữ liệu

--- sonha_purchase/models/x_hr_validate_rule.py
+++ sonha_purchase/models/x_hr_validate_rule.py
@@ -6,12 +6,12 @@ class XHrValidateRule(models.Model):
 
 # các trường comment là các trường cần thiết, có thể chưa có model liên kết
     name = fields.Char(string="Quy tắc phê duyệt")
-    # x_validate_title = fields.Many2one('x.hr.validate.person',string="Vai trò phê duyệt")
+    x_validate_title = fields.Many2one('x.hr.validate.person',string="Vai trò phê duyệt")
     x_sequence = fields.Integer(string="Trình tự")
     # x_validate_department = fields.Many2one('hr.department',string="Phòng/Ban phê duyệt")
     # x_validate_person = fields.Many2one('res.users',string="Người phê duyệt")
     x_validate_person_domain = fields.Char(string="domain Người phê duyệt")
-    # procedure_id = fields.Many2one('x.hr.validate.procedure',string='Quy trình')
+    procedure_id = fields.Many2one('x.hr.validate.procedure',string='Quy trình')
     x_method = fields.Selection(
         selection=[
             ('by_title','Vai trò'),
@@ -66,12 +66,12 @@ class XHrValidateRule(models.Model):
             for r in data:
                 records_to_create.append({
                     'name': r.get('name'),
-                    # 'x_validate_title': r.get('x_validate_title'),
+                    'x_validate_title': r.get('x_validate_title'),
                     'x_sequence': r.get('x_sequence'),
                     # 'x_validate_department': r.get('x_validate_department'),
                     # 'x_validate_person': r.get('x_validate_person'),
                     'x_validate_person_domain': r.get('x_validate_person_domain'),
-                    # 'procedure_id': r.get('procedure_id'),
+                    'procedure_id': r.get('procedure_id'),
                     'x_method': r.get('x_method'),
                     # 'x_limit': r.get('x_limit'),
                     # 'x_report_major_id': r.get('x_report_major_id'),
