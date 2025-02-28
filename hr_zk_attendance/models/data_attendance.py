00e0009ccb9dfe8e88f481de1069671f8f1acae2
sonhasoftware <sonha.software.1@gmail.com>
2025-02-28 10:09:13 +0700

    Update data_attendance.py

--- hr_zk_attendance/models/data_attendance.py
+++ hr_zk_attendance/models/data_attendance.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 from datetime import timedelta
+from dateutil.relativedelta import relativedelta
 
 
 class DataAttendance(models.Model):
@@ -11,7 +12,20 @@ class DataAttendance(models.Model):
     device_ip = fields.Char("Địa chỉ IP")
 
     def clone_attendance_data(self):
-        attendance_records = self.sudo().search([])
+
+        today = fields.Datetime.now()  # Lấy thời gian hiện tại
+        first_day_this_month = today.replace(day=1, hour=0, minute=0, second=0)  # Ngày đầu tháng, reset giờ về 00:00:00
+        first_day_last_month = first_day_this_month - relativedelta(months=1)  # Ngày đầu tháng trước
+
+        # Lấy ngày đầu tháng sau để làm mốc cho "<"
+        first_day_next_month = first_day_this_month + relativedelta(months=1)
+
+        # Tạo domain tìm kiếm
+        domain = [
+            ('date_time', '>=', first_day_last_month),  # Từ ngày đầu tháng trước
+            ('date_time', '<', first_day_next_month)  # Đến trước ngày đầu tháng sau
+        ]
+        attendance_records = self.sudo().search(domain)
         employee_model = self.env['hr.employee']
         master_attendance_model = self.env['master.data.attendance']
 
