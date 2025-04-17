05c930dc9754e983a730878e6e5d86a239025eaa
LongNguyn <157455862+LongNguyn@users.noreply.github.com>
2025-04-17 15:06:07 +0700

    update Đặt xe

--- sonha_book_car/wizard/wizard_return_card.py
+++ sonha_book_car/wizard/wizard_return_card.py
@@ -6,9 +6,11 @@ class WizardReturnCard(models.TransientModel):
 
     receive_people = fields.Many2one('hr.employee', string="Người nhận thẻ", required=True)
     receive_time = fields.Date("Ngày trả thẻ", required=True)
-    return_people = fields.Many2one('hr.employee', string="Người trả thẻ", required=True)
+    return_people = fields.Many2one('hr.employee', string="Người trả thẻ",
+                                    domain="[('department_id', '=', department_id)]", required=True)
 
     parent_id = fields.Many2one('book.car', string="Parent ID")
+    department_id = fields.Many2one('hr.department', string="Phòng ban")
 
     def action_confirm(self):
         self.parent_id.write({
@@ -19,3 +21,4 @@ class WizardReturnCard(models.TransientModel):
             'list_view_status': self.parent_id.list_view_status + " → Hoàn thành",
         })
         return {'type': 'ir.actions.act_window_close'}
+
