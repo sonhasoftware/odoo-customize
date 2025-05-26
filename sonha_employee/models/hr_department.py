d9b995b135a356ca4579f4e90aa2885ac858fea8
sonhasoftware <sonha.software.1@gmail.com>
2025-05-26 09:36:28 +0700

    update ssp

--- sonha_employee/models/hr_department.py
+++ sonha_employee/models/hr_department.py
@@ -5,3 +5,6 @@ class HrDepartment(models.Model):
 
     send_mail = fields.Boolean(string='Gửi mail', default=False)
     shift = fields.Many2one('config.shift', string="Ca làm việc")
+    over_time = fields.Selection([('hours', 'Hưởng giờ'),
+                                  ('date', 'Hưởng ngày')
+                                  ], string='Category', default='hours')
--- sonha_word_slip/models/overtime_rel.py
+++ sonha_word_slip/models/overtime_rel.py
@@ -8,4 +8,5 @@ class OvertimeRel(models.Model):
     date = fields.Date("Ngày", required=True)
     start_time = fields.Float("Thời gian bắt đầu", required=True)
     end_time = fields.Float("Thời gian kết thúc", required=True)
+    coefficient = fields.Float("Hệ số", default=1)
     overtime_id = fields.Many2one('register.overtime.update')
