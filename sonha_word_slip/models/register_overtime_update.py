6f49944eac18d4e85242f31e66d1718299d5eb8b
sonhasoftware <sonha.software.1@gmail.com>
2025-07-02 16:39:31 +0700

    Update register_overtime_update.py

--- sonha_word_slip/models/register_overtime_update.py
+++ sonha_word_slip/models/register_overtime_update.py
@@ -230,7 +230,7 @@ class RegisterOvertimeUpdate(models.Model):
                 time_ot = (ot.end_time - ot.start_time) * ot.coefficient
                 over_time += time_ot
             if r.type == 'one':
-                if r.department_id.manager_id.user_id.id == self.env.user.id or self.env.user.id == 1738:
+                if r.department_id.manager_id.user_id.id == self.env.user.id or self.env.user.id == 1738 or r.employee_id.parent_id.user_id.id == self.env.user.id:
                     r.status = 'done'
                     if r.employee_id.department_id.over_time == 'date':
                         r.employee_id.total_compensatory += over_time
@@ -239,7 +239,7 @@ class RegisterOvertimeUpdate(models.Model):
                 else:
                     raise ValidationError("Bạn không có quyền thực hiện hành động này")
             else:
-                if r.department_id.manager_id.user_id.id == self.env.user.id or self.env.user.id == 1738:
+                if r.department_id.manager_id.user_id.id == self.env.user.id or self.env.user.id == 1738 or r.employee_ids[-1].parent_id.user_id.id == self.env.user.id:
                     r.status = 'done'
                     for employee in r.employee_ids:
                         if employee.department_id.over_time == 'date':
