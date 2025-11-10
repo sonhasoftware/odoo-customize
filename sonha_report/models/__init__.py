b4352fa8fbe7504b59aad2dd368cee58d15b42bf
LongNguyn <157455862+LongNguyn@users.noreply.github.com>
2025-11-10 16:13:17 +0700

    update report ca + làm thêm

--- sonha_report/models/__init__.py
+++ sonha_report/models/__init__.py
@@ -1,7 +1,7 @@
 from . import master_data_report
 from . import employee_attendance_report
 from . import synthetic_work_report
-from . import employee_shift_report
+from . import sonha_word_report
 from . import leave_report
 from . import synthetic_leave_report
 from . import config_template_report
@@ -9,7 +9,6 @@ from . import reward_report
 from . import discipline_report
 from . import dismissal_report
 from . import word_slip_report
-from . import overtime_report
 from . import employee_report
 
 from . import leave_left
