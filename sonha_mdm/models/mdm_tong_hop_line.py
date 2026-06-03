f33a5ebc559b56ea20111969acf06928df63e4b8
sonhasoftware <sonha.software.1@gmail.com>
2026-06-03 14:49:39 +0700

    Call item API when adding unit lines

--- sonha_mdm/models/mdm_tong_hop_line.py
+++ sonha_mdm/models/mdm_tong_hop_line.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class MDMTongHopLine(models.Model):
@@ -13,3 +13,11 @@ class MDMTongHopLine(models.Model):
 
     dvt = fields.Many2one('mdm.dvt', string="Đơn vị tính", related='tong_hop_id.dvt', store=True)
     bom_sale = fields.Many2one('bom.sale',  string="Loại Bom Sale", related='tong_hop_id.bom_sale', store=True)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        if not self.env.context.get('skip_mdm_api_sync'):
+            for record in records.filtered('tong_hop_id'):
+                record.tong_hop_id.call_api_insert(record.tong_hop_id, line=record)
+        return records
