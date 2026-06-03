d88b0b3fe259f4c14e91773427bc39cc1f97fd10
sonhasoftware <sonha.software.1@gmail.com>
2026-06-03 15:18:33 +0700

    Update mdm_tong_hop_line.py

--- sonha_mdm/models/mdm_tong_hop_line.py
+++ sonha_mdm/models/mdm_tong_hop_line.py
@@ -9,7 +9,7 @@ class MDMTongHopLine(models.Model):
     ma_mdm = fields.Char(string='Mã MDM')
     ma_dv = fields.Char(string='Mã đơn vị')
     dvcs = fields.Many2one('res.company', string='ĐVCS')
-    ten = fields.Char(related='tong_hop_id.ten', string="Tên", stotr=True)
+    ten = fields.Char(related='tong_hop_id.ten', string="Tên", store=True)
 
     dvt = fields.Many2one('mdm.dvt', string="Đơn vị tính", related='tong_hop_id.dvt', store=True)
     bom_sale = fields.Many2one('bom.sale',  string="Loại Bom Sale", related='tong_hop_id.bom_sale', store=True)
