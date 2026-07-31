8294a5219cc0d6cdd5f915bc9bbcdde29ac9d419
sonhasoftware <sonha.software.1@gmail.com>
2026-07-31 17:01:24 +0700

    Update data_attendance.py

--- hr_zk_attendance/models/data_attendance.py
+++ hr_zk_attendance/models/data_attendance.py
@@ -49,44 +49,60 @@ class DataAttendance(models.Model):
         #             })
 
     def clone_data_mcc_old(self):
-        today = datetime.datetime.now()
-        if today.day <= 10:
-            start_date = today + timedelta(days=-10)
-        else:
-            start_date = today.replace(
-                day=1
-            )
-        end_date = today.replace(day=1) + relativedelta(months=1)
+        # today = datetime.datetime.now()
+        # if today.day <= 10:
+        #     start_date = today + timedelta(days=-10)
+        # else:
+        #     start_date = today.replace(
+        #         day=1
+        #     )
+        # end_date = today.replace(day=1) + relativedelta(months=1)
+        #
+        # start_date_text = start_date.strftime('%d/%m/%Y')
+        # end_date_text = end_date.strftime('%d/%m/%Y')
+        #
+        # self.env.cr.execute(
+        #     "CALL PR_DL_MCC_OLD(%s, %s);",
+        #     (start_date_text, end_date_text)
+        # )
+        # return True
 
-        start_date_text = start_date.strftime('%d/%m/%Y')
-        end_date_text = end_date.strftime('%d/%m/%Y')
+        today = fields.Datetime.now()
 
-        self.env.cr.execute(
-            "CALL PR_DL_MCC_OLD(%s, %s);",
-            (start_date_text, end_date_text)
+        start_date = (today - timedelta(days=6)).replace(
+            hour=0,
+            minute=0,
+            second=0,
+            microsecond=0
         )
-        return True
 
-        # attendance_records = self.sudo().search([
-        #     ('date_time', '>=', start_date),
-        #     ('date_time', '<=', end_date)
-        # ], order='date_time')
-        # employee_model = self.env['hr.employee']
-        # master_attendance_model = self.env['master.data.attendance']
-        #
-        # for record in attendance_records:
-        #     # Tìm nhân viên dựa vào mã chấm công
-        #     employee = employee_model.sudo().search([('device_id_num', '=', record.code)], limit=1)
-        #     if employee:
-        #         # Kiểm tra xem bản ghi đã tồn tại trong master.data.attendance chưa
-        #         existing_record = master_attendance_model.sudo().search([
-        #             ('employee_id', '=', employee.id),
-        #             ('attendance_time', '=', record.date_time - timedelta(hours=7))
-        #         ], limit=1)
-        #
-        #         if not existing_record:
-        #             # Tạo bản ghi mới trong bảng master.data.attendance
-        #             master_attendance_model.create({
-        #                 'employee_id': employee.id,
-        #                 'attendance_time': record.date_time - timedelta(hours=7),
-        #             })
+        end_date = today.replace(
+            hour=23,
+            minute=59,
+            second=59,
+            microsecond=999999
+        )
+
+        attendance_records = self.sudo().search([
+            ('date_time', '>=', start_date),
+            ('date_time', '<=', end_date)
+        ], order='date_time')
+        employee_model = self.env['hr.employee']
+        master_attendance_model = self.env['master.data.attendance']
+
+        for record in attendance_records:
+            # Tìm nhân viên dựa vào mã chấm công
+            employee = employee_model.sudo().search([('device_id_num', '=', record.code)], limit=1)
+            if employee:
+                # Kiểm tra xem bản ghi đã tồn tại trong master.data.attendance chưa
+                existing_record = master_attendance_model.sudo().search([
+                    ('employee_id', '=', employee.id),
+                    ('attendance_time', '=', record.date_time - timedelta(hours=7))
+                ], limit=1)
+
+                if not existing_record:
+                    # Tạo bản ghi mới trong bảng master.data.attendance
+                    master_attendance_model.create({
+                        'employee_id': employee.id,
+                        'attendance_time': record.date_time - timedelta(hours=7),
+                    })
