2a695d634f6a5271423d27becaf286c8216d6b50
sonhasoftware <sonha.software.1@gmail.com>
2026-08-21 09:40:21 +0700

    update code

---
+++ sonha_du_an/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sơn Hà dự án',
+    'version': '1.7',
+    'category': 'sonha du an',
+    'summary': 'sonha du an',
+    'website': 'https://hrm.sonha.com.vn/',
+    'description': "sonha du an",
+    'depends': ['base', 'project'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/nhap_lieu_du_an_views.xml',
+        'views/menu.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
---
+++ sonha_du_an/models/project_task.py
@@ -0,0 +1,14 @@
+from odoo import api, Command, fields, models, tools, SUPERUSER_ID, _, _lt
+
+
+class Task(models.Model):
+    _inherit = 'project.task'
+
+    noi_dung_cv = fields.Text("Nội dung công việc")
+    so_ngay_ht = fields.Float("Số ngày hoàn thành")
+    ngay_bat_dau = fields.Date("Ngày bắt đầu")
+    ngay_ket_thuc = fields.Date("Ngày kết thúc")
+    ngay_hoan_thanh = fields.Date("Ngày hoàn thành")
+    ns_lam = fields.Many2one('res.users', string="NS làm")
+    chu_so_huu = fields.Many2one('res.users', string="Chủ sở hữu")
+
