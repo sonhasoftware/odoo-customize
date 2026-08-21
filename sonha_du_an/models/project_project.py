27ccb0290d85bd7ed33208aafa169e916a3a429d
sonhasoftware <sonha.software.1@gmail.com>
2026-08-21 10:33:40 +0700

    Add parent child project task hierarchy

--- sonha_du_an/models/project_project.py
+++ sonha_du_an/models/project_project.py
@@ -1,4 +1,5 @@
-from odoo import api, Command, fields, models, _, _lt
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class Project(models.Model):
@@ -10,4 +11,43 @@ class Project(models.Model):
     nguoi_qlda = fields.Many2one('res.users', string="Người QLDA")
     ngay_kt_da = fields.Date("Ngày kết thúc DA")
     ngay_kt_chinh_sua = fields.Date("Ngày kết thúc chỉnh sửa")
+    parent_du_an_id = fields.Many2one(
+        'project.project',
+        string="Dự án cha",
+        index=True,
+        ondelete='restrict',
+        domain="[('id', '!=', id)]",
+    )
+    du_an_con_ids = fields.One2many(
+        'project.project',
+        'parent_du_an_id',
+        string="Dự án con",
+    )
+    child_task_ids = fields.Many2many(
+        'project.task',
+        compute='_compute_child_task_ids',
+        string="Nhiệm vụ theo dự án con",
+    )
+    is_parent_du_an = fields.Boolean(
+        string="Là dự án cha",
+        compute='_compute_is_parent_du_an',
+    )
 
+    @api.depends('du_an_con_ids')
+    def _compute_is_parent_du_an(self):
+        for project in self:
+            project.is_parent_du_an = bool(project.du_an_con_ids)
+
+    @api.depends('du_an_con_ids.task_ids')
+    def _compute_child_task_ids(self):
+        for project in self:
+            project.child_task_ids = project.du_an_con_ids.mapped('task_ids')
+
+    @api.constrains('parent_du_an_id')
+    def _check_parent_du_an_id(self):
+        for project in self:
+            parent = project.parent_du_an_id
+            while parent:
+                if parent == project:
+                    raise ValidationError("Dự án cha/con không được tạo vòng lặp.")
+                parent = parent.parent_du_an_id
