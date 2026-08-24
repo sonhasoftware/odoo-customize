91c3b33795edb00392dd818823edd128ff6660fa
sonhasoftware <sonha.software.1@gmail.com>
2026-08-24 10:19:49 +0700

    Revert "Merge pull request #615 from sonhasoftware/codex/cap-nhat-ma-e-phan-cap-du-an"
    This reverts commit 2b0a9daa6470738ffc0ae6647649f04fb8a9ef0e, reversing
    changes made to 728ec56667f30c56fab5b5ec48796c448e71a1f4.

--- sonha_du_an/models/project_task.py
+++ sonha_du_an/models/project_task.py
@@ -1,23 +1,10 @@
-from odoo import fields, models
+from odoo import api, Command, fields, models, tools, SUPERUSER_ID, _, _lt
 
 
 class Task(models.Model):
     _inherit = 'project.task'
 
-    cap = fields.Many2one(
-        'project.project',
-        string="Dự án con",
-        related='project_id',
-        store=True,
-        readonly=False,
-    )
-    parent_du_an_id = fields.Many2one(
-        'project.project',
-        string="Dự án cha",
-        related='project_id.parent_du_an_id',
-        store=True,
-        readonly=True,
-    )
+    cap = fields.Many2one('project.project', string="Cấp")
     noi_dung_cv = fields.Text("Nội dung công việc")
     so_ngay_ht = fields.Float("Số ngày hoàn thành")
     ngay_bat_dau = fields.Date("Ngày bắt đầu")
@@ -25,3 +12,4 @@ class Task(models.Model):
     ngay_hoan_thanh = fields.Date("Ngày hoàn thành")
     ns_lam = fields.Many2one('res.users', string="NS làm")
     chu_so_huu = fields.Many2one('res.users', string="Chủ sở hữu")
+
