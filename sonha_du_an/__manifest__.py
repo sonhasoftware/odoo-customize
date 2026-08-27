436131bc9bad942d8e235beb9ebaa2793fbaf12a
sonhasoftware <sonha.software.1@gmail.com>
2026-08-27 14:41:59 +0700

    update code

--- sonha_du_an/__manifest__.py
+++ sonha_du_an/__manifest__.py
@@ -13,6 +13,7 @@
         'views/nhap_lieu_du_an_views.xml',
         'views/project_task_status_views.xml',
         'views/group_du_an_views.xml',
+        'wizard/project_task_pending_wizard_views.xml',
         'views/menu.xml'
     ],
     'installable': True,
