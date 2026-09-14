ed5fbb087af3cd3f93d5c0aa590a9bca64642ad2
sonhasoftware <sonha.software.1@gmail.com>
2026-09-14 16:02:25 +0700

    update code

--- sonha_du_an/wizard/bao_cao_du_an_wizard.py
+++ sonha_du_an/wizard/bao_cao_du_an_wizard.py
@@ -28,6 +28,6 @@ class BaoCaoDuAnWizard(models.TransientModel):
             'type': 'ir.actions.act_window',
             'name': _('Báo cáo dự án'),
             'res_model': 'sonha.du.an.bao.cao',
-            'view_mode': 'pivot,tree,form',
+            'view_mode': 'tree,form',
             'context': {'search_default_group_by_parent': 1},
         }
